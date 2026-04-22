b44d9f01f079130e82c03d77fe557a8639ddcb2c
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2026-04-21 21:31:00 -0600

    Logging basico

--- api-checker-tool/utils/client.py
+++ api-checker-tool/utils/client.py
@@ -1,9 +1,20 @@
 import requests
+import logging
+
+logging.basicConfig(
+    filename="reports/test.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 BASE_URL = "https://jsonplaceholder.typicode.com"
 
-    
+
 def send_get(endpoint):
     url = f"{BASE_URL}{endpoint}"
+    logging.info(f"Sending GET request to {url}")
+    
     response = requests.get(url)
+
+    logging.info(f"Received status: {response.status_code}")
     return response
