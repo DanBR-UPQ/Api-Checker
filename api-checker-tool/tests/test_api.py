19c7baf095c79a2ba0614e3f3a2193b7dde979b0
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2026-04-21 21:40:45 -0600

    Validación de response time

--- api-checker-tool/tests/test_api.py
+++ api-checker-tool/tests/test_api.py
@@ -2,7 +2,8 @@ import json
 import pytest
 
 from utils.client import send_get
-from utils.validators import validate_status_code, validate_keys
+from utils.validators import validate_status_code, validate_keys, validate_response_time
+
 
 with open("config/test_cases.json") as f:
     test_cases = json.load(f)
@@ -18,3 +19,6 @@ def test_api_cases(case):
     if "expected_keys" in case:
         data = response.json()
         assert validate_keys(data, case["expected_keys"])
+    
+    if "max_response_time" in case:
+        assert validate_response_time(response, case["max_response_time"])
--- api-checker-tool/utils/validators.py
+++ api-checker-tool/utils/validators.py
@@ -5,3 +5,6 @@ def validate_status_code(response, expected_code):
 
 def validate_keys(response_json, expected_keys):
     return all(key in response_json for key in expected_keys)
+
+def validate_response_time(response, max_ms):
+    return response.elapsed.total_seconds() * 1000 < max_ms
